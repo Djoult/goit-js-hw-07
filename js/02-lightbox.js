e7fe41a722e01df3977bb240a6e88f396e732806
Djoult <55453354+Djoult@users.noreply.github.com>
2023-02-20 19:57:52 +0200

    SinpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+  <a class="gallery__item" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>`
+  )
+  .join("");
+var lightbox = new SimpleLightbox(".gallery a", {
+  navText: ["&#11178;", "&#11179;"],
+  captions: true,
+  captionDelay: 250,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionClass: "",
+  //   closeText: "&#10015;", краще не розкоментовувати :)
+  closeText: "&#10803",
+  showCounter: false,
+});
