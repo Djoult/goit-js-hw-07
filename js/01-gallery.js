c59c5af80e05b2c7a952a2ef7c30f2aa50173445
Djoult <55453354+Djoult@users.noreply.github.com>
2023-02-20 16:47:00 +0200

    Task 1 Gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+  )
+  .join("");
+gallery.addEventListener("click", openGalleryImageModal);
+function openGalleryImageModal(event) {
+  event.preventDefault();
+  //   if (event.target.nodeName !== "IMG") {
+  //     return;
+  //   }
+  if (event.target == event.currentTarget) {
+    return;
+  }
 
-console.log(galleryItems);
+  const currentImg = event.target;
+  console.log(currentImg);
+  const currentLink = currentImg.dataset.source;
+  const instance = basicLightbox.create(`
+    
+          <img width="1400" height="900" src="${currentLink}">
+  
+  `);
+
+  instance.show();
+  document.addEventListener("keydown", closeModal);
+}
+function closeModal(e) {
+  e.preventDefault();
+  if (e.key === "Escape") {
+    console.log("closed");
+    const imageToClose = document.querySelector(".basicLightbox--img");
+    imageToClose.remove();
+    document.removeEventListener("keydown", closeModal);
+    // const close = function (elem, next) {
+    //   elem.classList.remove("basicLightbox--visible");
+
+    //   setTimeout(() => {
+    //     if (visible(elem) === false) return next();
+
+    //     elem.parentElement.removeChild(elem);
+
+    //     return next();
+    //   }, 410);
+
+    //   return true;
+    // };
+  }
+}
